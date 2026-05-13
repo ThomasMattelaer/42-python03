c8a8f15fa2dc4f83d10127a4957424f9963a4628
ThomasMattelaer <mattelaer.thomas@hotmail.com>
2026-05-13 11:27:33 +0200

    exo1 done

--- ex1/ft_score_analytics.py
+++ ex1/ft_score_analytics.py
@@ -2,28 +2,33 @@ import sys
 
 
 def ft_score_analytics() -> None:
-
     argc = len(sys.argv)
-    score = 0
     tab = []  # type: list
-    i = 0
+    i = 1
+    print("===Player Score Analytics ===\n")
     while (i < argc):
         try:
-            tab[i] = int(sys.argv[i])
-            score += int(sys.argv[i])
+            tab.append(int(sys.argv[i]))
         except ValueError:
             print(f"Invalid Parameter: '{sys.argv[i]}'")
         i += 1
-    max_value = max(tab)
-    min_value = min(tab)
-    range = (max_value - min_value)
-    print(f""" ===Player Score Analytics\n
-Total Players: {argc}
+    length = len(tab)
+    if (length > 0):
+        score = sum(tab)
+        max_value = max(tab)
+        min_value = min(tab)
+        range = (max_value - min_value)
+        average_score = score / length
+        print(f"""Score processed: {tab}
+Total Players: {length}
 Total score: {score}
+Average score: {average_score}
 High score: {max_value}
 Lowest score: {min_value}
 Score range: {range}
 """)
+    else:
+        print(f"No scores provided. Usage: {sys.argv[0]} <score1> <score2> ..")
 
 
 if __name__ == "__main__":
